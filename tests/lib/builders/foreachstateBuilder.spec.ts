4564ce018ed10f7011b0d4433f61d83188aba74d
Antonio Mendoza Pérez <antmendoza@gmail.com>
2021-05-18 17:53:19 +0200

    foreachstateBuilder format
    Signed-off-by: Antonio Mendoza Pérez <antmendoza@gmail.com>

--- tests/lib/builders/foreachstateBuilder.spec.ts
+++ tests/lib/builders/foreachstateBuilder.spec.ts
@@ -1,35 +1,27 @@
-import {foreachstateBuilder} from '../../../src';
+import { foreachstateBuilder } from '../../../src';
 
-describe("foreachstateBuilder" , ()=> {
+describe('foreachstateBuilder', () => {
+  it('should build an object with default usedForCompensation = false', () => {
+    const forEachEstate = foreachstateBuilder()
+      .name('forEachEstate')
+      .inputCollection('${ .expressions }')
+      .iterationParam('singleexpression')
+      .workflowId('workflowId')
+      .end(true)
+      .build();
 
+    expect(forEachEstate.usedForCompensation).toBeFalse();
+  });
 
-	it("should build an object with default usedForCompensation = false", ()=> {
-		
-		const forEachEstate = foreachstateBuilder()
-			.name("forEachEstate")
-			.inputCollection('${ .expressions }')
-			.iterationParam('singleexpression')
-			.workflowId("workflowId")
-			.end(true)
-			.build();
-		
-		expect(forEachEstate.usedForCompensation).toBeFalse();
-		
-	});
-	
-	
-	
-	it("should allow overwrite usedForCompensation default value", ()=> {
-		
-		const forEachEstate = foreachstateBuilder()
-			.name("forEachEstate")
-			.inputCollection('${ .expressions }')
-			.iterationParam('singleexpression')
-			.workflowId("workflowId")
-			.usedForCompensation(true)
-			.build();
-		
-		expect(forEachEstate.usedForCompensation).toBeTrue();
-		
-	});
-})
+  it('should allow overwrite usedForCompensation default value', () => {
+    const forEachEstate = foreachstateBuilder()
+      .name('forEachEstate')
+      .inputCollection('${ .expressions }')
+      .iterationParam('singleexpression')
+      .workflowId('workflowId')
+      .usedForCompensation(true)
+      .build();
+
+    expect(forEachEstate.usedForCompensation).toBeTrue();
+  });
+});
