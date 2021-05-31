caf026d2ab99a73fc9ecdbe26561a84c4568b257
Antonio Mendoza Pérez <antmendoza@gmail.com>
2021-05-31 12:31:52 +0200

    validate method return proprietary class
    Signed-off-by: Antonio Mendoza Pérez <antmendoza@gmail.com>

--- src/lib/workflow-validator.ts
+++ src/lib/workflow-validator.ts
@@ -15,15 +15,16 @@
  *
  */
 
-import { DefinedError, ValidateFunction } from 'ajv';
+import { ValidateFunction } from 'ajv';
 import { Specification } from './definitions';
 import { validators } from './validators';
 
 export class WorkflowValidator {
   /** The validation errors after running validate(), if any */
-  errors: DefinedError[] | never[] = [];
+  errors: ValidationError[] | never[] = [];
   /** The validate function */
   private validateFn: ValidateFunction<Specification.Workflow>;
+
   /**
    * Creates a new WorkflowValidator for the provided workflow
    * @param {Workflow} workflow The workflow to validate
@@ -31,13 +32,22 @@ export class WorkflowValidator {
   constructor(private workflow: Specification.Workflow) {
     this.validateFn = validators.get('Workflow') as ValidateFunction<Specification.Workflow>;
   }
+
   /**
    * Validates the workflow, populates the errors if any
    * @returns {boolean} If the workflow is valid or not
    */
   validate(): boolean {
     const isValid = this.validateFn(this.workflow);
-    this.errors = this.validateFn.errors as DefinedError[];
+    if (this.validateFn.errors) {
+      this.errors = this.validateFn.errors.map(
+        (error) => new ValidationError(`invalid: ${error.instancePath} | ${error.schemaPath} | ${error.message}`)
+      );
+    }
     return isValid;
   }
+}
+
+export class ValidationError {
+  constructor(readonly message: string) {}
 }
---
+++ tests/workflow-validator.spec.ts
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2021-Present The Serverless Workflow Specification Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+import { ValidationError, WorkflowValidator } from '../src/';
+import { Workflow } from '../src/lib/definitions/workflow';
+
+describe('workflow-validator', () => {
+  it('should return errors instance of ValidationError if the workflow provided is not valid', () => {
+    // @ts-ignore
+    const workflow = {
+      id: 'helloworld',
+      name: 'Hello World Workflow',
+      version: '1.0',
+      description: 'Inject Hello World',
+      start: 'Hello State',
+      states: [],
+    } as Workflow;
+
+    const workflowValidator = new WorkflowValidator(workflow);
+    expect(workflowValidator.errors.length).toBe(0);
+
+    workflowValidator.validate();
+    expect(workflowValidator.errors.length).toBe(1);
+    expect(workflowValidator.errors[0].constructor === ValidationError).toBeTruthy(
+      'Expected errors to be instance of ValidationError'
+    );
+  });
+
+  it('should have no errors if the workflow is valid', () => {
+    const workflow = {
+      id: 'helloworld',
+      version: '1.0',
+      name: 'Hello World Workflow',
+      description: 'Inject Hello World',
+      start: 'Hello State',
+      states: [
+        {
+          name: 'Hello State',
+          type: 'inject',
+          data: {
+            result: 'Hello World!',
+          },
+          end: true,
+        },
+      ],
+    } as Workflow;
+    
+    const workflowValidator = new WorkflowValidator(workflow);
+    workflowValidator.validate();
+    expect(workflowValidator.errors.length).toBe(0);
+  });
+});
