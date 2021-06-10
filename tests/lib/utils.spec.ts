8c2515b6e6b4d8d9db4aaf1fb0ee6512a5cdadd2
Antonio Mendoza Pérez <antmendoza@gmail.com>
2021-06-10 13:01:31 +0200

    improve validation message by adding data
    Signed-off-by: Antonio Mendoza Pérez <antmendoza@gmail.com>

---
+++ tests/lib/utils.spec.ts
@@ -0,0 +1,12 @@
+import { validate } from '../../src/lib/utils';
+
+describe('validate', () => {
+  it('should throw an error containing the provided data', () => {
+    expect( () => validate('End', "any text")).toThrowError(/any text/);
+  });
+
+  it('should return true for valid objects', () => {
+    expect(validate('End', false)).toBeTruthy("Expected function validate to return true for valid objects");
+  });
+  
+});
