793a1bcc06dadfe0715c882b4bc9591bfe1b3151
Antonio Mendoza Pérez <antmendoza@gmail.com>
2022-01-11 00:30:55 +0100

    fix WorkflowValidator
    Signed-off-by: Antonio Mendoza Pérez <antmendoza@gmail.com>

--- src/lib/workflow-validator.ts
+++ src/lib/workflow-validator.ts
@@ -34,11 +34,12 @@ export class WorkflowValidator {
    */
   constructor(private workflow: Specification.Workflow) {
     const validateFn = validators.get('Workflow') as ValidateFunction<Specification.Workflow>;
-    validateFn(JSON.parse(JSON.stringify(this.workflow.normalize())));
+    const normalizedWf = this.workflow.normalize();
+    validateFn(JSON.parse(JSON.stringify(normalizedWf)));
     if (validateFn.errors) {
       this.errors = validateFn.errors.map((error) => {
         const message = `Workflow is invalid: ${error.instancePath} | ${error.schemaPath} | ${error.message}
-        data: ${JSON.stringify(workflow, null, 4)}`;
+        data: ${JSON.stringify(normalizedWf, null, 4)}`;
         return new ValidationError(message);
       });
     }
